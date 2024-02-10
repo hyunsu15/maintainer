0f84f5083cf5e015cbebbe5054ac85570ab6cf1f
hyunsu15 <48500411+hyunsu15@users.noreply.github.com>
2024-02-10 20:46:47 +0900

    test : 사장님이 로그인한경우 상품 커서조회를 할수 있다 추가
    사장님이 로그인하지 않은경우 상품은 커서 조회되지 않는다
    
    사장님이 로그인한 경우 상품은 커서조회를 할수 있다
    
    사장님이 로그인한 경우라도, 상품개수가_0개면 NOCONTENT를 반환한다
    
    사장님이 로그인한 경우라도 다른사장님의 상품은 조회되지않는다

--- src/test/java/com/example/maintainer/product/adapter/in/web/ProductControllerTest.java
+++ src/test/java/com/example/maintainer/product/adapter/in/web/ProductControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.maintainer.product.adapter.in.web;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import com.example.maintainer.ControllerTestMustExtends;
@@ -8,6 +9,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -25,15 +28,15 @@ class ProductControllerTest extends ControllerTestMustExtends {
   @Test
   void 사장님이_로그인하지않은경우_상품은_저장되지_않는다() throws Exception {
     String product = "{\n"
-        + "      \"category\":\"asdf\",\n"
-        + "        \"salePrice\":1234,\n"
-        + "        \"cost\": 500,\n"
-        + "        \"name\":\"test\",\n"
-        + "        \"description\":\"test\",\n"
-        + "        \"barcode\":12341234,\n"
-        + "        \"size\":\"small\",\n"
-        + "        \"expiredDate\":\"2023-02-12T00:00:00\"\n"
-        + "    }";
+        + "    \"category\":\"asdf\",\n"
+        + "    \"salePrice\":1234,\n"
+        + "    \"cost\": 500,\n"
+        + "    \"name\":\"test\",\n"
+        + "    \"description\":\"test\",\n"
+        + "    \"barcode\":12341234,\n"
+        + "    \"size\":\"small\",\n"
+        + "    \"expiredDate\":\"2023-02-12T00:00:00\"\n"
+        + "}";
     String token = 만기된토큰;
     MockHttpServletResponse response = api호출(상품저장(product, token));
     Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
@@ -56,8 +59,130 @@ class ProductControllerTest extends ControllerTestMustExtends {
     Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
   }
 
+  @Test
+  void 사장님이_로그인하지_않은경우_상품은_커서_조회되지_않는다() throws Exception {
+    String canUseToken = 회원가입로그인성공후토큰반환();
+    for (int i = 0; i < 15; i++) {
+      로그인후_상품등록(canUseToken);
+    }
+    String token = 만기된토큰;
+    MockHttpServletResponse response = api호출(상품커서조회(0L, token));
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "1,1",
+      "2,2",
+      "3,3",
+      "4,4",
+      "5,5",
+      "6,6",
+      "7,7",
+      "8,8",
+      "9,9",
+      "10,10",
+      "11,10",
+      "12,10",
+      "13,10",
+      "14,10"
+  })
+  void 사장님이_로그인한_경우_상품은_커서조회를_할수_있다(int length, int expectSize) throws Exception {
+    String canUseToken = 회원가입로그인성공후토큰반환();
+    for (int i = 0; i < length; i++) {
+      로그인후_상품등록(canUseToken);
+    }
+    String token = canUseToken;
+    MockHttpServletResponse response = api호출(상품커서조회(0L, token));
+    CustomResponse<ProductSearchWithNextCursorIdResponse> result = objectMapper.readValue(
+        response.getContentAsString(),
+        new TypeReference<CustomResponse<ProductSearchWithNextCursorIdResponse>>() {
+        });
+
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    Assertions.assertThat(result.getData().productSearches().size()).isEqualTo(expectSize);
+  }
+
+  @Test
+  void 사장님이_로그인한_경우라도_상품갯수가_0개면_NO_CONTENT를_반환한다() throws Exception {
+    String canUseToken = 회원가입로그인성공후토큰반환();
+    String token = canUseToken;
+    MockHttpServletResponse response = api호출(상품커서조회(0L, token));
+
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "1,1,1",
+      "2,1,1",
+      "3,1,1",
+      "4,1,1",
+      "5,1,1",
+      "2,2,2",
+      "3,2,2",
+      "4,2,2",
+      "5,2,2",
+      "4,3,3",
+      "5,3,3",
+      "5,4,4",
+  })
+  void 사장님이_로그인한_경우라도_다른사장님의_상품은_조회되지않는다(int otherCreate, int iCreate, int expectSize)
+      throws Exception {
+    String canUseToken = 회원가입로그인성공후토큰반환();
+    for (int i = 0; i < otherCreate; i++) {
+      로그인후_상품등록(canUseToken);
+    }
+    String token = 회원가입로그인성공후토큰반환("010-7890-7890");
+    for (int i = 0; i < iCreate; i++) {
+      로그인후_상품등록(token);
+    }
+    MockHttpServletResponse response = api호출(상품커서조회(0L, token));
+    CustomResponse<ProductSearchWithNextCursorIdResponse> result = objectMapper.readValue(
+        response.getContentAsString(),
+        new TypeReference<CustomResponse<ProductSearchWithNextCursorIdResponse>>() {
+        });
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    Assertions.assertThat(result.getData().productSearches().size()).isEqualTo(expectSize);
+
+  }
+
+  @Test
+  void 다른_사장님이_올린_경우는_상품이_조회되지_않는다() throws Exception {
+    String canUseToken = 회원가입로그인성공후토큰반환();
+    String token = canUseToken;
+    MockHttpServletResponse response = api호출(상품커서조회(0L, token));
+
+    Assertions.assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+  }
+
+  void 로그인후_상품등록(String token) throws Exception {
+    String product = "{\n"
+        + "      \"category\":\"asdf\",\n"
+        + "        \"salePrice\":1234,\n"
+        + "        \"cost\": 500,\n"
+        + "        \"name\":\"test\",\n"
+        + "        \"description\":\"test\",\n"
+        + "        \"barcode\":12341234,\n"
+        + "        \"size\":\"small\",\n"
+        + "        \"expiredDate\":\"2023-02-12T00:00:00\"\n"
+        + "    }";
+    api호출(상품저장(product, token));
+  }
+
   private String 회원가입로그인성공후토큰반환() throws Exception {
     String phoneNumber = "010-1234-1234";
+    String password = "1234";
+
+    api호출(회원가입(phoneNumber, password));
+    return objectMapper.readValue(
+            api호출(로그인(phoneNumber, password)).getContentAsString(),
+            new TypeReference<CustomResponse<String>>() {
+            })
+        .getData();
+  }
+
+  private String 회원가입로그인성공후토큰반환(String phoneNumber) throws Exception {
     String password = "1234";
 
     api호출(회원가입(phoneNumber, password));
@@ -93,5 +218,11 @@ class ProductControllerTest extends ControllerTestMustExtends {
         .header("X-USER-ID", token)
         .contentType(MediaType.APPLICATION_JSON)
         .content(request);
+  }
+
+  private RequestBuilder 상품커서조회(long cursorId, String token) {
+    return get("/product/list")
+        .param("cursorId", String.valueOf(cursorId))
+        .header("X-USER-ID", token);
   }
 }
